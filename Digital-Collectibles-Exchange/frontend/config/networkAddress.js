85b2012211f4fda8b47283f198eaff645c41c5df
Chandan Kumar Shaw <chandan.kumar.shaw@sap.com>
2024-10-20 14:03:08 +0530

    add csdp token holesky testnet address

--- Digital-Collectibles-Exchange/frontend/config/networkAddress.js
+++ Digital-Collectibles-Exchange/frontend/config/networkAddress.js
@@ -1,3 +1,4 @@
 export const nftMarketplaceAddress = "0x5639410db1ded8FfBc454CB696ba0866F181340A";
 export const nftAddress = "0xe1d9831bBDBD890a0066684D20972c5d2E4e10aE";
+export const csdpAddress = '0x007aa784FcD58C556Ec2C6491Afb6D3059494e4D';
 export const rpcProviderUrl = 'https://holesky.infura.io/v3/ad2130d177e64c04b12da5a7b2c64bc9';
