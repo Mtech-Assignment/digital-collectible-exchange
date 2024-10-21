afff0ac0dd6eafe8efeba0f483b0e79b05bb601a
Chandan Kumar Shaw <--global>
2024-10-21 20:25:38 +0530

    Add a UI to buy CSDP token

---
+++ Digital-Collectibles-Exchange/frontend/components/buy-csdp/csdp-token.jsx
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import {
+  csdpAddress,
+} from "../../config/networkAddress";
+import CSDPAbi from "../../abi/CSDP.json";
+import Web3Modal from "web3modal";
+import { useRouter } from "next/router";
+import BtnMain from "../../subcomponents/btns/BtnMain";
+import { AiOutlineArrowUp, } from "react-icons/ai";
+import Input from "./Input";
+
+
+export default function CSDPToken() {
+  const router = useRouter();
+
+  const [_, setLoading] = useState(false);
+  const [currentBalance, setCurrentBalance] = useState(0.0);
+  const [csdpTokenAmount, setCsdpTokenAmount] = useState(0);
+  const [priceInEther, setPriceInEther] = useState(0);
+  const [isPurchasing, setisPurchasing] = useState(false);
+
+  const loadCSDPBalance = async () => {
+    setLoading(true);
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    await provider.send("eth_requestAccounts", []); // Prompts user to connect wallet
+    const address = await signer.getAddress();
+    const CSDPTokenContract = new ethers.Contract(
+      csdpAddress,
+      CSDPAbi.abi,
+      provider
+    );
+    const currentBalance = await CSDPTokenContract.balanceOf(address);
+    currentBalance = currentBalance / (10 ** 18);
+    console.log(currentBalance);
+    setCurrentBalance(currentBalance);
+    setLoading(false);
+  };
+
+  const buyCSDP = async () => {
+    setisPurchasing(true);
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+
+    const signer = provider.getSigner();
+    const CSDPTokenContract = new ethers.Contract(
+      csdpAddress,
+      CSDPAbi.abi,
+      signer
+    );
+
+    CSDPTokenContract.on("Transfer", (f, t, value) => {
+      value = (value / (10 ** 18));
+      setCurrentBalance(currentBalance+value);
+    });
+
+    const etherPriceInUint = ethers.utils.parseUnits(`${priceInEther}`, "ether");
+
+    const transaction = await CSDPTokenContract.sendToken(
+      { value: etherPriceInUint }   // sending ether to contract to get a token
+    );
+    await transaction.wait();
+    setisPurchasing(false);
+  };
+
+  useEffect(() => {
+    loadCSDPBalance();
+  }, []);
+
+  return (
+    <div>
+      <h1>Buy CSDP using Ether</h1>
+      <h3>Current balance: {currentBalance} CSDP</h3>
+      <div>
+        <Input
+          id="csdp-amount"
+          placeholder="input upto 15 decimal places"
+          label="Token Amount"
+          value={csdpTokenAmount}
+          onChange={(e) => {
+            setCsdpTokenAmount(e.target.value);
+            setPriceInEther((e.target.value/1000));
+          }}
+        />
+        <Input
+            id="csdp-price-in-ether"
+            placeholder="e.g 1/2/3..."
+            label="Ether Amount"
+            value={priceInEther}
+            onChange={(e) => {
+              setPriceInEther(e.target.value);
+              setCsdpTokenAmount(e.target.value*1000);
+            }}
+        />
+        {!isPurchasing && <BtnMain
+          text="Buy Now"
+          icon={<AiOutlineArrowUp className="text-2xl" />}
+          className="text-lg w-full"
+          onClick={() => buyCSDP()}
+        />}
+      </div>
+    </div>
+  );
+}
--- Digital-Collectibles-Exchange/frontend/config/networkAddress.js
+++ Digital-Collectibles-Exchange/frontend/config/networkAddress.js
@@ -1,4 +1,4 @@
-export const nftMarketplaceAddress = "0xF20FE5A0bD216d76D902B3f2165523E6Eb664eeb";
-export const nftAddress = "0x310D2c738B2974AD4312da35B4361793Cf4E3B86";
-export const csdpAddress = '0x007aa784FcD58C556Ec2C6491Afb6D3059494e4D';
+export const nftMarketplaceAddress = "0x81ECB2962382106EE0092fd63EF4f71461aB22e2";
+export const nftAddress = "0x6C4bAC3184156b6d4114085573dF065B3F913714";
+export const csdpAddress = '0xF32D4d7d997A74130b24EF3C187f18e221A4b535';
 export const rpcProviderUrl = 'https://holesky.infura.io/v3/ad2130d177e64c04b12da5a7b2c64bc9';
