5c095ba45c301e90ad3788aa6aa6de0504a795ae
Shoib Ansari <shoib.ansari@sap.com>
2024-10-27 09:34:34 +0530

    my owned nft error handled

--- Digital-Collectibles-Exchange/frontend/components/my-items/MyItems.jsx
+++ Digital-Collectibles-Exchange/frontend/components/my-items/MyItems.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { ethers } from "ethers";
-import { nftAddress, nftMarketplaceAddress } from "../../config/networkAddress";
+import React, {useEffect, useState} from "react";
+import {ethers} from "ethers";
+import {nftAddress, nftMarketplaceAddress} from "../../config/networkAddress";
 import NFTAbi from "../../abi/NFT.json";
 import NFTMarketplaceAbi from "../../abi/NFTMarketplace.json";
 import axios from "axios";
@@ -17,42 +17,47 @@ export default function MyItems() {
   const loadMyNFTs = async () => {
     setLoading(true);
     const web3Modal = new Web3Modal();
-    const connection = await web3Modal.connect();
-    const provider = new ethers.providers.Web3Provider(connection);
-    const signer = provider.getSigner();
 
-    const nftContract = new ethers.Contract(nftAddress, NFTAbi.abi, signer);
-    const nftMarketPlaceContract = new ethers.Contract(
-      nftMarketplaceAddress,
-      NFTMarketplaceAbi.abi,
-      signer
-    );
-    
-    const data = await nftMarketPlaceContract.getOwnerListedItems();
+    try {
+      const connection = await web3Modal.connect();
+      const provider = new ethers.providers.Web3Provider(connection);
+      const signer = provider.getSigner();
 
-    const allItems = await Promise.all(
-      data?.map(async (i) => {
-        let convertedPrice = ethers.utils.formatUnits(
-          i.price.toString(),
-          18
-        );
-        const tokenUri = await nftContract.tokenURI(i.tokenId);
-        const metaData = await axios.get(tokenUri);
-        let item = {
-          price: convertedPrice,
-          tokenId: i.tokenId.toNumber(),
-          seller: i.seller,
-          owner: i.owner,
-          image: metaData.data.image,
-          name: metaData.data.name,
-          description: metaData.data.description,
-        };
-        return item;
-      })
-    );
-    setAllNFTs(allItems);
-    console.log(allNFTs);
-    setLoading(false);
+      const nftContract = new ethers.Contract(nftAddress, NFTAbi.abi, signer);
+      const nftMarketPlaceContract = new ethers.Contract(
+          nftMarketplaceAddress,
+          NFTMarketplaceAbi.abi,
+          signer
+      );
+
+      const data = await nftMarketPlaceContract.getOwnerListedItems();
+
+      const allItems = await Promise.all(
+          data?.map(async (i) => {
+            let convertedPrice = ethers.utils.formatUnits(i.price.toString(), 18);
+            const tokenUri = await nftContract.tokenURI(i.tokenId);
+            const metaData = await axios.get(tokenUri);
+            return {
+              price: convertedPrice,
+              tokenId: i.tokenId.toNumber(),
+              seller: i.seller,
+              owner: i.owner,
+              image: metaData.data.image,
+              name: metaData.data.name,
+              description: metaData.data.description,
+            };
+          })
+      );
+
+      setAllNFTs(allItems);
+    } catch (error) {
+      console.error("Error loading NFTs:", error);
+      alert("Failed to load NFTs. Please check your wallet connection or try again later.");
+      // Optionally reset allNFTs to an empty array or maintain previous state
+      setAllNFTs([]);
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
