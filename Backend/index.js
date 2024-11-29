ec1542d5b1ea8a983870113678434c6bc91b8257
I528581 <prashant.prashant01@sap.com>
2024-11-29 13:02:49 +0530

    added swagger specs

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,34 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const swaggerJSDoc = require('swagger-jsdoc')
+
+const swaggerUi =require('swagger-ui-express')
+const YAML = require('yamljs');
+
+
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+// const option={
+//     definition:{
+//         openapi:'3.0.0',
+//         info:{
+//             title:'NFT API Project',
+//             version:'1.0.0',
+//         },
+//         servers:[
+//             {
+//                 api:'http://localhost:3002/'
+//             }
+//         ]
+//     },
+//     apis:['./routes/v1/nftRoutes.js']
+// }
+
+// const swaggerSpec=swaggerJSDoc(option)
+// router.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerSpec))
+
+
 
 require('dotenv').config();
 
@@ -13,6 +41,9 @@ const PORT = process.env.PORT || 3002;
 
 // Enable CORS for all routes
 app.use(cors());
+
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Middleware
 app.use(bodyParser.json());
